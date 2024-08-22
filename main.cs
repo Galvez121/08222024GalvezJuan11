e0564019545174a7f292eca4fd35409a475eafe3
Juan Galvez <55637706+Galvez121@users.noreply.github.com>
2024-08-22 15:15:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string userName = "Lucinda Potter";
+        string birthday = "6/24/1992";
+        string workNumber = "000-000-0101";
+        string mobileNumber = "000-000-0189";
+        Console.WriteLine(userName);
+        Console.WriteLine(birthday);
+        Console.WriteLine("work " + workNumber);
+        Console.WriteLine("cell " + mobileNumber);
+
+    }
+}
